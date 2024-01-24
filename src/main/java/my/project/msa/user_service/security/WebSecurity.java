b4b89579bfe45088a4e76041c42ea8d991447ae4
MINmin-e <weluv.ww@gmail.com>
2024-01-24 16:46:47 +0900

    ~ 시큐리티 진행중

--- src/main/java/my/project/msa/user_service/security/WebSecurity.java
+++ src/main/java/my/project/msa/user_service/security/WebSecurity.java
@@ -2,23 +2,19 @@ package my.project.msa.user_service.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
-@EnableWebSecurity
-public class WebSecurity {
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.csrf().disable().formLogin().disable();
-        return httpSecurity
-                .authorizeHttpRequests(
-                        authorize -> authorize
-                                .requestMatchers("/user-service/users").permitAll()
-                                .requestMatchers("/login").permitAll()
-                                .requestMatchers("/actuator/**").permitAll()
-                )
-                .build();
-    }
-}
+//@Configuration
+//@EnableWebSecurity
+//public class WebSecurity {
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .csrf(Customizer.withDefaults())
+//                .aut
+//
+//    }
+//}
