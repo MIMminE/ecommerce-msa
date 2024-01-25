44888dbbf73000d9c4df9e8aa7bba8153aad2024
MINmin-e <weluv.ww@gmail.com>
2024-01-25 16:44:22 +0900

    Initial commit

---
+++ src/main/java/my/project/msa/catalog_service/CatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package my.project.msa.catalog_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogServiceApplication.class, args);
+	}
+
+}
